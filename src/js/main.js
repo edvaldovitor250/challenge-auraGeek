a22eca3469df08e4a66b32d9087af68fe3315802
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-05-12 21:57:00 -0300

    fix: default image size

--- src/js/main.js
+++ src/js/main.js
@@ -4,28 +4,29 @@ function limparFormulario() {
 
 
 
-    function adicionarCarro() {
-        var nome = document.getElementById('nome').value;
-        var valor = document.getElementById('valor').value;
-        var imagem = document.getElementById('imagem').value;
+function adicionarCarro() {
+    var nome = document.getElementById('nome').value;
+    var valor = document.getElementById('valor').value;
+    var imagem = document.getElementById('imagem').value;
 
-        var card = document.createElement('div');
-        card.classList.add('card', 'mb-3');
-        card.innerHTML = `
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${imagem}" class="img-fluid rounded-start" alt="${nome}">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${nome}</h5>
-                        <p class="card-text">Valor: R$ ${valor}</p>
-                    </div>
+    var card = document.createElement('div');
+    card.classList.add('card', 'mb-3');
+    card.innerHTML = `
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${imagem}" class="img-fluid rounded-start" alt="${nome}" style="max-width: 100%; height: auto;">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${nome}</h5>
+                    <p class="card-text">Valor: R$ ${valor}</p>
                 </div>
             </div>
-        `;
+        </div>
+    `;
 
-        document.getElementById('carros').appendChild(card);
+    document.getElementById('carros').appendChild(card);
 
-        document.getElementById('carForm').reset();
-    }
+    document.getElementById('carForm').reset();
+}
+
